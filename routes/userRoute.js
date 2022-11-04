b389b31a2104084ab9fb1246175166b4450f3bc9
TEMILOLUWA <bigdevtemy@gmail.com>
2022-11-04 12:13:57 +0100

    Service

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,6 @@ import {indexpage,registerUser} from '../controllers/userController.js'
 import {auth} from '../middleware/authMiddleware.js'
 const route = express.Router();
 
-route.get('/',auth,indexpage);
+route.get('/',indexpage);
 route.post('/registeruser',registerUser);
 export default route;
