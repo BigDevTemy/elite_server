be3c5a1d35066056263420632604ab5c8c8d6166
TEMILOLUWA <bigdevtemy@gmail.com>
2022-11-06 06:45:19 +0100

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,12 @@ const indexpage = ((req,res)=>{
 
 const registerUser  = ((req,res)=>{
 const body = req.body;
+
+    console.log(req.body);
+    res.status(200).send({
+        'message':'Saved',
+        'status':true
+    })
     users.findOne({email:body.email},async (err,docs)=>{
         if(err){
             res.status(400).send(err);
@@ -19,6 +25,8 @@ const body = req.body;
         }
         else{
               const userCreated = new users(body);
+
+
               const salt = await bcrypt.genSalt(10);
               userCreated.password = await bcrypt.hash(userCreated.password, salt);
               userCreated.save().then((doc) => res.status(201).send(doc));
