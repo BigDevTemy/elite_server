2724b680c1a7da31c3efc8bf7a0ed3ace8740a45
TEMILOLUWA <bigdevtemy@gmail.com>
2022-11-12 03:32:36 +0100

    okay

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {indexpage,createCategory,createLevel,createDiscovery,allDiscovery} from '../controllers/adminController.js'
+import {auth} from '../middleware/authMiddleware.js'
 const route = express.Router();
 
 route.get('/',indexpage);
 route.post('/create/category',createCategory);
 route.post('/create/level',createLevel);
 route.post('/create/discovery',createDiscovery);
-route.get('/all/discovery',allDiscovery);
+route.get('/all/discovery',auth,allDiscovery);
 export default route;
