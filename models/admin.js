a08559073a5adbbd9d3285adb90cd20595cfb895
TEMILOLUWA <bigdevtemy@gmail.com>
2023-08-13 13:57:59 +0100

    kop

--- models/admin.js
+++ models/admin.js
@@ -15,6 +15,10 @@ const adminSchema = mongoose.Schema({
         type:'String',
         required:[true]
     },
+    roleId:{
+        type:'String',
+        required:[true]
+    },
     
     status:{
         type:'String',
